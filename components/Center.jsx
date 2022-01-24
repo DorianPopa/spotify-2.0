02a015ae9c0c14d1764dc2cf98ffdd788e23d2bc
Popa Dorian <popa.dorian98@gmail.com>
2022-01-24 16:13:32 +0200

    Implemented the song list and remote-play functionality

--- components/Center.jsx
+++ components/Center.jsx
@@ -1,16 +1,21 @@
 import { ChevronDownIcon } from "@heroicons/react/outline";
-import { useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import React, { useEffect, useState } from "react";
 import { shuffle } from "lodash";
-import { useRecoilValue } from "recoil";
-import { playlistIdState } from "../atoms/playlistAtom";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { playlistIdState, playlistState } from "../atoms/playlistAtom";
+import useSpotify from "../hooks/useSpotify";
+import Songs from "./Songs";
 
 const colors = ["from-blue-500", "from-red-500", "from-pink-500"];
 
 const Center = () => {
+	const spotifyApi = useSpotify();
+
 	const { data: session } = useSession();
 	const [color, setColor] = useState(null);
 	const playlistId = useRecoilValue(playlistIdState);
+	const [playlist, setPlaylist] = useRecoilState(playlistState);
 
 	// 2:26:12
 
@@ -18,10 +23,25 @@ const Center = () => {
 		setColor(shuffle(colors).pop());
 	}, [playlistId]);
 
+	useEffect(() => {
+		spotifyApi
+			.getPlaylist(playlistId)
+			.then((data) => {
+				setPlaylist(data.body);
+			})
+			.catch((err) =>
+				console.log("Something went wrong while getting playlist", err)
+			);
+	}, [spotifyApi, playlistId]);
+
 	return (
-		<div className="flex-grow">
+		<div className="flex-grow h-screen overflow-y-scroll scrollbar-hide">
 			<header className="absolute top-5 right-8">
-				<div className="flex items-center bg-black text-white space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2">
+				<div
+					className="flex items-center bg-black text-white space-x-3 
+					opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2"
+					onClick={signOut}
+				>
 					<img
 						className="rounded-full w-10 h-10"
 						src={session?.user.image}
@@ -35,8 +55,22 @@ const Center = () => {
 			<section
 				className={`flex items-end space-x-7 bg-gradient-to-b to-black ${color} h-80 text-white p-8`}
 			>
-				{/* <img src="" alt="" /> */}
+				<img
+					className="h-44 w-44 shadow-2xl"
+					src={playlist?.images?.[0].url}
+					alt=""
+				/>
+				<div>
+					<p>Playlist</p>
+					<h1 className="text-2xl md:text-3xl xl:text-5xl font-bold">
+						{playlist?.name}
+					</h1>
+				</div>
 			</section>
+
+			<div>
+				<Songs />
+			</div>
 		</div>
 	);
 };
--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Head from "next/head";
 import Sidebar from "../components/Sidebar";
 import Center from "../components/Center";
+import { getSession } from "next-auth/react";
 
 export default function Home() {
 	return (
@@ -14,4 +15,14 @@ export default function Home() {
 			<div>{/* Player */}</div>
 		</div>
 	);
+}
+
+export async function getServerSideProps(context) {
+	const session = await getSession(context);
+
+	return {
+		props: {
+			session,
+		},
+	};
 }
